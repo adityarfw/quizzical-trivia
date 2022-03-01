8ba3e2d7edb7409b95fc3763347619d4af4ed5f7
adityarfw <aditya.rajkumar@wisetail.com>
2022-02-28 20:47:01 -0500

    Reafactored Code in Quiz Card and Quiz Page

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,54 +1,33 @@
 import React from 'react';
-import { nanoid } from 'nanoid';
 
-const QuizCard = () => {
-  const [quizData, setQuizData] = React.useState([]);
-  React.useEffect(() => {
-    fetch(
-      'https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple'
-    )
-      .then((res) => res.json())
-      .then((data) => setQuizData(data.results));
-  }, []);
+const QuizCard = (props) => {
+  const { quiz_question, correct_answer, incorrect_answers } = props;
 
   const handleClick = (event) => {};
 
-  // JSON data provides correct answer in arr[0]
-  // Shuffling options
-  const shuffleAnswers = (arr) => {
-    return arr.sort(() => Math.random() - 0.5);
-  };
-
   let optionsArray = [];
 
-  // Get quiz data and map through each quiz element
-  // Get correct answer and push to an array
-  // Get incorrect answers and push to the same array
-  // Shuffle the elements
-  // Display the elements below question
-  // Clear the temporary options array
-  const quizQA = quizData.map(
-    (quiz) => (
-      (optionsArray.length = 0),
-      optionsArray.push(quiz.correct_answer),
-      quiz.incorrect_answers.map((incAns) => optionsArray.push(incAns)),
-      (
-        <div>
-          <h4 className='quizcard--question'>{quiz.question}</h4>
-          <div className='answer--container'>
-            {shuffleAnswers(optionsArray).map((ans) => (
-              <button onClick={handleClick} className='answer--option'>
-                {ans}
-              </button>
-            ))}
-          </div>
-          <hr className='quiz--card--break' />
-        </div>
-      )
-    )
-  );
+  const addOptions = (option) => {
+    optionsArray.push(option);
+  };
 
-  return <div className='container--quizcard'>{quizQA}</div>;
+  const shuffleAnswers = (arr) => {
+    return arr.sort(() => Math.random() - 0.25);
+  };
+
+  return (
+    <div className='container--quizcard'>
+      <h4 className='quizcard--question'>{quiz_question}</h4>
+      <div className='answer--container'>
+        {addOptions(correct_answer)}
+        {incorrect_answers.map((incAns) => addOptions(incAns))}
+        {shuffleAnswers(optionsArray).map((options) => (
+          <button className='answer--option'>{options}</button>
+        ))}
+      </div>
+      <hr className='quiz--card--break' />
+    </div>
+  );
 };
 
 export default QuizCard;
--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import QuizCard from './QuizCard';
+import { nanoid } from 'nanoid';
 
 const QuizPage = () => {
+  const [quizData, setQuizData] = React.useState([]);
+  const [questionCard, setQuestionCard] = React.useState([]);
+  React.useEffect(() => {
+    fetch(
+      'https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple'
+    )
+      .then((res) => res.json())
+      .then((data) => setQuizData(data.results));
+  }, []);
+
+  const card = {};
+
+  // Get quiz data and map through each quiz element
+  // Get correct answer and push to an array
+  // Get incorrect answers and push to the same array
+  // Shuffle the elements
+  // Display the elements below question
+  // Clear the temporary options array
+
+  const quizQA = quizData.map((quiz) => (
+    <QuizCard
+      quiz_question={quiz.question}
+      correct_answer={quiz.correct_answer}
+      incorrect_answers={quiz.incorrect_answers}
+      key={nanoid()}
+    />
+  ));
+
   return (
     <div className='quizpage--container'>
-      <QuizCard />
+      {quizQA}
       <button className='quizpage--button'>Check Answers</button>
     </div>
   );
